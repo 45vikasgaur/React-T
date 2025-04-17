4e800f85d65b4753d7c981e9f83d3a84740de025
45vikasgaur <gondvikas383@gmail.com>
2025-04-17 14:06:57 +0530

    set of DAY6

--- Day7/backend-basic/index7.js
+++ Day7/backend-basic/index7.js
@@ -7,11 +7,46 @@ app.get('/', (req, res) => {
 })
 
 app.get('/abc', (req, res) => {
-  const a = req.query;
+  const a = req.query.a;
+  const b = req.query.b;
   console.log(a);
-  res.send('Path ABC');
+  const sum = parseInt(a) + parseInt(b);
+  res.send('Path ABC! ' + sum);
 })
 
+app.get('/getData', (req, res) => {
+  res.json ({
+    success: true,
+    Massage: 'Data fetched successfully',
+    myData: data,
+  });
+});
+
+app.post('/abcPost', (req, res) => {
+  const a = req.query.a;
+  const b = req.query.b;
+  console.log(a);
+  const sum = parseInt(a) + parseInt(b);
+  res.send('Method Podt : ' + sum);
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+const data =[
+{
+  id: 1,
+  name: 'a', 
+}
+,
+{
+  id: 2,
+  name: 'b',
+}
+,
+{
+  id: 3,
+  name: 'c',
+}
+]
